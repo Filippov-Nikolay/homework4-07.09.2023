589f821fbabe71c535aea616df0f61c84b83a136
Filippov-Nikolay <filippovnikolay195@gmail.com>
2023-09-10 21:29:07 +0300

    Улучшено меню, добавлены проверки

--- homework4 07.09.2023/main.cpp
+++ homework4 07.09.2023/main.cpp
@@ -7,11 +7,13 @@ int main() {
     setlocale(0, "");
     system("chcp 1251");
 
-    int numberSub = 2;
+    int numberSub = 0;
     int numberDelate = 0;
     int userChoice = 0;
     int key = 0;
-    int minMenuItem = 1, maxMenuItem = 6;
+    int minMenuItem = 1, maxMenuItem = 7;
+
+    bool flag = false;
 
     char buff[100];
 
@@ -56,6 +58,7 @@ int main() {
                     cout << endl;
                 }
 
+                flag = true;
             }
             else if (userChoice == 2) {
                 numberSub = subscribers->ResizeArray(subscribers, numberSub);
@@ -63,54 +66,74 @@ int main() {
                     subscribers[i].Input();
                     cout << endl;
                 }
+
+                flag = true;
             }
-            else if (userChoice == 3) {
-                cout << "Поиск по ФИО: " << endl;
-                cout << "Введите фамилию: ";
-                cin >> buff;
-                lastName = new char[strlen(buff) + 1];
-                strcpy_s(lastName, strlen(buff) + 1, buff);
 
-                cout << "Введите имя: ";
-                cin >> buff;
-                firstName = new char[strlen(buff) + 1];
-                strcpy_s(firstName, strlen(buff) + 1, buff);
+            if (!flag)
+                cout << "Выберите сначало 1 или 2 пункт!" << endl << endl;
+            else {
+                if (userChoice == 3) {
+                    cout << "Поиск по ФИО: " << endl;
+                    cout << "Введите фамилию: ";
+                    cin >> buff;
+                    lastName = new char[strlen(buff) + 1];
+                    strcpy_s(lastName, strlen(buff) + 1, buff);
 
-                cout << "Введите отчество: ";
-                cin >> buff;
-                surName = new char[strlen(buff) + 1];
-                strcpy_s(surName, strlen(buff) + 1, buff);
+                    cout << "Введите имя: ";
+                    cin >> buff;
+                    firstName = new char[strlen(buff) + 1];
+                    strcpy_s(firstName, strlen(buff) + 1, buff);
 
-                key = subscribers->SearchByFullname(subscribers, numberSub, firstName, lastName, surName);
+                    cout << "Введите отчество: ";
+                    cin >> buff;
+                    surName = new char[strlen(buff) + 1];
+                    strcpy_s(surName, strlen(buff) + 1, buff);
 
-                cout << "KEY: " << key << endl;
+                    key = subscribers->SearchByFullname(subscribers, numberSub, firstName, lastName, surName);
 
-                if (key == -1)
-                    cout << "Ключ не найден!" << endl;
-                else
-                    subscribers[key].Print();
-            }
-            else if (userChoice == 4) {
-                for (int i = 0; i < numberSub; i++) {
-                    subscribers[i].Print();
-                    cout << endl;
+                    cout << "KEY: " << key << endl;
+
+                    if (key == -1)
+                        cout << "Ключ не найден!" << endl;
+                    else
+                        subscribers[key].Print();
+                }
+                else if (userChoice == 4) {
+                    if (numberSub == 0) {
+                        cout << "Нет абонентов для вывода!" << endl << endl;
+                    }
+                    else {
+                        for (int i = 0; i < numberSub; i++) {
+                            subscribers[i].Print();
+                            cout << endl;
+                        }
+                    }
+                }
+                else if (userChoice == 5) {
+                    subscribers->SaveToFile(subscribers, numberSub);
+                    cout << "Данные записаны в файл" << endl << endl;
+                }
+                else if (userChoice == 6) {
+                    subscribers->LoadFromFile();
+                }
+                else if (userChoice == 7) {
+                    cout << "Кол-во абонентов: " << numberSub << endl;
+
+                    do {
+                        cout << "Введите абонента которого нужно удалить: ";
+                        cin >> numberDelate;
+
+                        if (numberDelate > numberSub || numberDelate <= 0)
+                            cout << "Вы ввели не допустимый диапозон!" << endl << endl;                   
+                    } while (numberDelate > numberSub || numberDelate <= 0);
+                    numberDelate--;
+
+                    numberSub = subscribers->DelateSubscriber(subscribers, numberSub, numberDelate);
+                    cout << "Абонент удалён!" << endl << endl;
                 }
             }
-            else if (userChoice == 5) {
-                subscribers->SaveToFile(subscribers, numberSub);
-                cout << "Данные записаны в файл" << endl << endl;
-            }
-            else if (userChoice == 6) {
-                subscribers->LoadFromFile();
-            }
-            else if (userChoice == 7) {
-                cout << "Введите пользователя которого нужно удалить: ";
-                cin >> numberDelate;
-
-                numberSub = subscribers->DelateSubscriber(subscribers, numberSub, numberDelate);
-            }
         }
-
     } while (userChoice != 0 || userChoice < minMenuItem || userChoice > maxMenuItem);
 
     return 0;
